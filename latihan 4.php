3a67d8f6ed2b146e94168dc339d9355d8f2d6a13
Eldrick Trifaldo <edrikmaster643@gmail.com>
2026-03-14 22:54:56 +0700

    Create latihan 4.php

---
+++ latihan 4.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+<title>Penggunaan Switch Case</title>
+</head>
+
+<body>
+Hari ini :
+<?php
+$nama_hari = date("l");
+
+switch ($nama_hari) {
+
+case "Sunday":
+    print("Minggu <br>");
+    print("Waktu untuk istirahat");
+    break;
+
+case "Monday":
+    print("Senin <br>");
+    print("Meeting awal minggu jam 08.00");
+    break;
+
+case "Tuesday":
+    print("Selasa <br>");
+    print("Pembukaan Workshop Diklat");
+    break;
+
+case "Wednesday":
+    print("Rabu <br>");
+    print("Seminar Launching Window Vista di JHCC");
+    break;
+
+case "Thursday":
+    print("Kamis <br>");
+    print("Pertemuan dengan Mahasiswa");
+    break;
+
+case "Friday":
+    print("Jumat <br>");
+    print("Jogging bersama");
+    break;
+
+default:
+    print("Sabtu <br>");
+    print("Survey harga ke Dusit, Mangga Dua");
+}
+?>
+</body>
+</html>
