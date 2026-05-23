bbc20130238b2b6fefe99caabca1baff2689b429
Eldrick Trifaldo <edrikmaster643@gmail.com>
2026-05-24 00:06:49 +0700

    Add files via upload

---
+++ pertemuan 7/latihan5.php
@@ -0,0 +1,10 @@
+<?php
+$program = array('Bobo','Doraemon','Spiderman');
+list($Majalah, $Komik, $Film) = $program;
+
+echo "Jenis Buku & Hiburan :<br />";
+echo "Cerpen : $Majalah<br />";
+echo "Cerita Bergambar : $Komik<br />";
+echo "Bioskop : $Film";
+
+?>
---
+++ pertemuan 7/latihan8.php
@@ -0,0 +1,6 @@
+<?php
+$var = array('18', '11', '2010');
+$tanggal = join("/", $var);
+echo "$tanggal";
+
+?>
