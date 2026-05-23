cd8a726dd1dc90ccebc73bfbfdfb8cd0a3b8358c
Eldrick Trifaldo <edrikmaster643@gmail.com>
2026-05-24 00:06:31 +0700

    Add files via upload

---
+++ pertemuan6/latihan1.php
@@ -0,0 +1,16 @@
+<?php
+$a[0] = 1;
+$a[1] = 3;
+$a[2] = 5;
+
+$jumlah = count($a);
+print "Jumlah array a = $jumlah <br>";
+
+$b["buah"] = "semangka";
+$b["sayur"] = "wortel";
+$b["daging"] = "ayam";
+$b["utama"] = "nasi";
+
+$jumlah = sizeof($b);
+print "Jumlah array b = $jumlah <br>";
+?>
